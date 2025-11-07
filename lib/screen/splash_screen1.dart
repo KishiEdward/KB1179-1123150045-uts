80262be7961c515c331032f2267fe365e6c30a5d
KishiEdward <dzidanrh@gmail.com>
2025-11-07 19:51:05 +0700

    membuat splash screen 1

---
+++ lib/screen/splash_screen1.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen1 extends StatelessWidget {
+  const SplashScreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 250,
+              height: 250,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Color.fromARGB(255, 108, 244, 67),
+                
+              ),
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
