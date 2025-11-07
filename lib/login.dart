ec457c698ebd73247edae8ff4bddbe23d872b6aa
KishiEdward <dzidanrh@gmail.com>
2025-11-07 19:46:17 +0700

    membuat login dart (belum finish)

---
+++ lib/login.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 10),
+              
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
